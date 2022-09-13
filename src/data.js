85c83107b40c09a8eda10677fa61115b58ebf1c2
Ryan J. Parker <ryan.jos.parker@gmail.com>
2022-09-13 15:08:01 -0700

    add signs to data.js and add zodiac icons

---
+++ src/data.js
@@ -0,0 +1,52 @@
+export const signs = [
+  {
+    name: 'Capricorn',
+    dates: 'December 22 - January 19',
+  },
+  {
+    name: 'Aquarius',
+    dates: 'January 20 - February 18',
+  },
+  {
+    name: 'Pisces',
+    dates: 'February 19 - March 20',
+  },
+  {
+    name: 'Aries',
+    dates: 'March 21 - April 19',
+  },
+  {
+    name: 'Taurus',
+    dates: 'April 20 - May 20',
+  },
+  {
+    name: 'Gemini',
+    dates: 'May 21 - June 20',
+  },
+  {
+    name: 'Cancer',
+    dates: 'June 21 - July 22',
+  },
+  {
+    name: 'Leo',
+    dates: 'July 23 - August 22',
+  },
+  {
+    name: 'Virgo',
+    dates: 'August 23 - September 22',
+  },
+  {
+    name: 'Libra',
+    dates: 'September 23 - October 22',
+  },
+  {
+    name: 'Scorpio',
+    dates: 'October 23 - November 21',
+  },
+  {
+    name: 'Sagittarius',
+    dates: 'November 22 - December 21',
+  },
+];
+
+export default signs;
